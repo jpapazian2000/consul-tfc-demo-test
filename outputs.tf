96c7db408a70aacfa0ec3b4a6cb3dfe64a8d7c5c
jpapazian2000 <jpapazian@hashicorp.com>
2021-02-10 20:35:04 +0100

    dsqd

--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,6 @@
-output "project" {
-    value = data.google_client_config.default.project
-}
-output "region" {
-    value = data.google_client_config.default.region
-}
-output "zone" {
-    value = data.google_client_config.default.zone
-}
 
-output "zones_available" {
-    value = data.google_compute_zones.available.names
-}
 
-output "cluster_primary_endpoint" {
-    value = google_container_cluster.gke_cluster.endpoint
-}
 
-output "certificate" {
-    value = google_container_cluster.gke_cluster.master_auth.0.cluster_ca_certificate
-}
 /*
 output "namespace" {
     value = kubernetes_namespace.consul.metadata[0].name
