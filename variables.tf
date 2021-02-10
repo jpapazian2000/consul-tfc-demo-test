900827a26de99392a5816c67415088aaab66b6bc
jpapazian2000 <jpapazian@hashicorp.com>
2021-02-10 20:27:12 +0100

    fff

--- variables.tf
+++ variables.tf
@@ -10,6 +10,10 @@ variable "workers_count" {
   default = "3"
 }
 
+variable "gke_cluster_name" {
+    description = "name of the cluster"
+}
+
 variable "region" {
     description = "region of google"
 }
