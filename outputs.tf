fb04d3379dd9a4863e41b01064710787472775cb
jpapazian2000 <jpapazian@hashicorp.com>
2021-02-11 16:48:11 +0100

    added ip outpy-ut

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,9 @@ output "cluster_name" {
     value = module.gke_cluster.cluster_name
 }
 
-
+output "ip du cluster kubernetes" {
+    value = module.gke_cluster.cluster_primary_endpoint
+}
 /*
 output "namespace" {
     value = kubernetes_namespace.consul.metadata[0].name
