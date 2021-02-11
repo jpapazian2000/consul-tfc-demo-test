13fac13d9fcc0a308c4e5a2c66504c87e9a0b735
jpapazian2000 <jpapazian@hashicorp.com>
2021-02-11 18:00:01 +0100

    added consul helm

---
+++ k8s_conf/consul.tf
@@ -0,0 +1,28 @@
+resource "kubernetes_namespace" "consul_ns" {
+    metadata {
+      name = var.k8s_ns
+    }
+}
+
+resource "helm_release" "consul" {
+    depends_on = [ kubernetes.namespace.consul_ns ]
+    name = "gke-consul"
+    chart = "${path.module}/consul-helm"
+    namespace = kubernetes_namespace.consul_ns.metadata.0.name
+
+  set {
+    name  = "global.name"
+    value = "consul"
+  }
+
+  set {
+    name  = "server.replicas"
+    value = "2"
+  }
+
+  set {
+    name  = "server.bootstrapExpect"
+    value = "2"
+  }
+}
+
--- k8s_conf/main.tf
+++ k8s_conf/main.tf
@@ -8,48 +8,11 @@ provider "kubernetes" {
     cluster_ca_certificate = base64decode(var.cluster_ca_cert)
 }
 
-resource "kubernetes_namespace" "consul_ns" {
-    metadata {
-      name = var.k8s_ns
+provider "helm" {
+    kubernetes {
+        load_config_file = false
+        host = var.cluster_endpoint
+        token = data.google_client_config.default.access_token
+        cluster_ca_certificate = base64decode(var.cluster_ca_cert)
     }
-}
-
-resource "kubernetes_deployment" "consul_depl"{
-    metadata {
-        name = "consul-depl"
-        namespace = kubernetes_namespace.consul_ns.metadata.0.name
-    }
-    spec {
-        replicas = 2
-        selector {
-            match_labels = {
-                app = "consul"
-            }
-        }
-        template {
-            metadata {
-                labels = {
-                    app = "consul"
-                }
-            }
-            spec {
-                container {
-                    image = "nginx:1.19.4"
-                    name = "nginx"
-
-                    resources {
-                        limits = {
-                            memory = "512M"
-                            cpu = "1"
-                        }
-                        requests = {
-                            memory = "256M"
-                            cpu = "50m"
-                        }
-                    }
-                }
-            }
-        }
-    }
-
 }
