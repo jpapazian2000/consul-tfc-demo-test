4e4f9e34b2244679ef456b54cbdd99759643da01
jpapazian2000 <jpapazian@hashicorp.com>
2021-02-11 18:03:48 +0100

    oki

--- k8s_conf/main.tf
+++ k8s_conf/main.tf
@@ -3,16 +3,15 @@ data "google_client_config" "default" {
 }
 
 provider "kubernetes" {
-    host = var.cluster_endpoint
-    token = data.google_client_config.default.access_token
-    cluster_ca_certificate = base64decode(var.cluster_ca_cert)
+    host                        = var.cluster_endpoint
+    token                       = data.google_client_config.default.access_token
+    cluster_ca_certificate      = base64decode(var.cluster_ca_cert)
 }
 
 provider "helm" {
     kubernetes {
-        load_config_file = false
-        host = var.cluster_endpoint
-        token = data.google_client_config.default.access_token
+        host                   = var.cluster_endpoint
+        token                  = data.google_client_config.default.access_token
         cluster_ca_certificate = base64decode(var.cluster_ca_cert)
     }
 }
