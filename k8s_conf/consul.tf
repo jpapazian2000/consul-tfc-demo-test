03f994a03b5c4d4113f67145fb6f91fb89abc35b
jpapazian2000 <jpapazian@hashicorp.com>
2021-02-12 18:02:59 +0100

    chaged repo helm chart

--- k8s_conf/consul.tf
+++ k8s_conf/consul.tf
@@ -7,7 +7,7 @@ resource "kubernetes_namespace" "consul_ns" {
 resource "helm_release" "consul" {
     depends_on = [ kubernetes_namespace.consul_ns ]
     name = "gke-consul"
-    repository = "https://helm.releases.hashicorp.com/"
+    repository = "${path.module}/consul-helm"
     chart = "consul"
     namespace = kubernetes_namespace.consul_ns.metadata.0.name
 
