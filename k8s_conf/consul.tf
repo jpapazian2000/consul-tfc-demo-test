8c6ad133fb8e1a20dd146adc66ed847299437802
jpapazian2000 <jpapazian@hashicorp.com>
2021-02-15 15:45:11 +0100

    modified output

--- k8s_conf/consul.tf
+++ k8s_conf/consul.tf
@@ -18,4 +18,20 @@ resource "helm_release" "consul" {
     }
 
 }
+resource "time_sleep" "wait_60s" {
+  depends_on = [
+    helm_release.consul
+  ]
+  create_duration = "1s"  
+}
 
+data "kubernetes_service" "consul-ui" {
+  depends_on = [
+    helm_release.consul,
+    time_sleep.wait_60s
+  ]
+  metadata {
+    name = "consul-ui"
+    namespace = kubernetes_namespace.consul_ns.metadata.0.name
+  }
+}
