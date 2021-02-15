371e42eaf8bb5aeba73a2faf6210bb6208bd7e33
jpapazian2000 <jpapazian@hashicorp.com>
2021-02-15 17:20:55 +0100

    ok

--- k8s_conf/outputs.tf
+++ k8s_conf/outputs.tf
@@ -1,3 +1,3 @@
 output "ui_service" {
-    value = [data.kubernetes_service.consul-ui.status.0.load_balancer.0.ingress.0.hostname]
+    value = [data.kubernetes_service.consul-ui.status.0.load_balancer.0.ingress.0.ip]
 }
